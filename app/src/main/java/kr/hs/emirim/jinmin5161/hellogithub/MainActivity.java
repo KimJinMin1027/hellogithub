3a24f51d6f1dd33eacb56269b1b6dc6321c0c163
KimJinMin <jinmin5161@e-mirim.hs.kr>
2017-03-28 20:54:49 +0900

    #2 버튼 누르면 폭파

--- app/src/main/java/kr/hs/emirim/jinmin5161/hellogithub/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/jinmin5161/hellogithub/MainActivity.java
@@ -2,12 +2,24 @@ package kr.hs.emirim.jinmin5161.hellogithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnHello = (Button) findViewById(R.id.btnHello);
+        btnHello.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "콰과광", Toast.LENGTH_SHORT).show(); //show를 호출해야 버튼이 보임.
+
     }
 }
